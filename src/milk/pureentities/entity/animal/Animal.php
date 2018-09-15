0a5d8028b6946a7963370cfa6e22b289c3018ae7
Leinne <nno88551@naver.com>
2018-09-15 22:07:01 +0900

    구 엔진 브랜치 등록
    현재 실행 불가능, 차츰 수정 예정

--- src/milk/pureentities/entity/animal/Animal.php
+++ src/milk/pureentities/entity/animal/Animal.php
@@ -1,16 +1,9 @@
 <?php
 
-declare(strict_types=1);
-
 namespace milk\pureentities\entity\animal;
 
-use milk\pureentities\entity\EntityBase;
 use pocketmine\entity\Ageable;
 
-abstract class Animal extends EntityBase implements Ageable{
-
-    public function isBaby() : bool{
-        return $this->getGenericFlag(self::DATA_FLAG_BABY);
-    }
+interface Animal extends Ageable{
 
 }
